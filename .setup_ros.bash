e9ea18b79a1152ba2a7e92f8453e63913c08d2a3
Jack O'Quin <jack.oquin@gmail.com>
2014-07-30 12:22:45 -0500

    ROS environment and install updates

--- .setup_ros.bash
+++ .setup_ros.bash
@@ -1,52 +1,46 @@
 ## bash setup for ROS development, invoke via "source" command
 
-alias rosenv="env | sort | egrep 'ROS|CATKIN|CMAKE|GAZEBO'"
-alias rdi="roscd; rosdep install --from-paths src --ignore-src --rosdistro=hydro"
-alias ws='source $ROS_WORKSPACE/devel/setup.bash'
+alias e="env | sort | egrep 'ROS|CATKIN|CMAKE'"
+alias g='source /opt/ros/groovy/setup.bash'
+alias h='source /opt/ros/hydro/setup.bash'
+alias i='source /opt/ros/indigo/setup.bash'
+alias ws='source ~/ros/ws/devel/setup.bash'
+
+## global ROS environment settings
+export ROS_EMAIL=jack.oquin@gmail.com
+export ROS_HOME=~/.ros
+export ROS_MASTER_URI=http://$HOSTNAME:11311
+export ROSCONSOLE_CONFIG_FILE=$ROS_HOME/config/rosconsole.config
 
 ## set ROS overlay name (W is a symlink)
-OVR=W
-export WS=$(readlink -f ~/ros/$OVR)
-
-if [ -r $WS/devel/setup.bash -o -r $WS/devel_isolated/setup.bash -o -r $WS/setup.bash ]
-then
-    export ROS_MASTER_URI=http://$HOSTNAME:11311
-
-    # catkin workspaces are set up differently
-    if [ -r $WS/devel/setup.bash ]
-    then    source $WS/devel/setup.bash
-            export CATKIN_TEST_RESULTS_DIR=~/ros/$OVR/build/test_results
-    else
-        if [ -r $WS/devel_isolated/setup.bash ]
-        then
-            source $WS/devel_isolated/setup.bash
-            export CATKIN_TEST_RESULTS_DIR=~/ros/$OVR/build_isolated/test_results
-        else
-            source $WS/setup.bash
-        fi
-    fi
-
-    export ROS_HOME=~/.ros
-    export ROSCONSOLE_CONFIG_FILE=$ROS_HOME/config/rosconsole.config
-    export ROS_EMAIL=jack.oquin@gmail.com
-    export ROS_WORKSPACE=$WS
-    export GAZEBO_MODEL_PATH=~/ros/gazebo/gazebo_models:${GAZEBO_MODEL_PATH}
-
-    # add art_run/bin to $PATH
-    #if [ $(which rospack) ]
-    #then ART_RUN=$(rospack find art_run 2>/dev/null)
-    #fi
-    #if [ "$ART_RUN" != "" ] && [ -d $ART_RUN/bin ]
-    #then export PATH=$ART_RUN/bin:$PATH
-    #fi
-else                            # no ROS workspace
-    if [ -r /opt/ros/hydro/setup.bash ]
-    then
-        source /opt/ros/hydro/setup.bash
-    else                        # ROS hydro not installed
-        if [ -r /opt/ros/indigo/setup.bash ]
-        then
-            source /opt/ros/indigo/setup.bash
-        fi
-    fi
-fi
+###OVR=W
+###export WS=$(readlink -f ~/ros/$OVR)
+###
+###if [ -r $WS/devel/setup.bash -o -r $WS/devel_isolated/setup.bash -o -r $WS/setup.bash ]
+###then
+###
+###    # catkin workspaces are set up differently
+###    if [ -r $WS/devel/setup.bash ]
+###    then    source $WS/devel/setup.bash
+###            export CATKIN_TEST_RESULTS_DIR=$WS/build/test_results
+###    else
+###        if [ -r $WS/devel_isolated/setup.bash ]
+###        then
+###            source $WS/devel_isolated/setup.bash
+###            export CATKIN_TEST_RESULTS_DIR=$WS/build_isolated/test_results
+###        else
+###            source $WS/setup.bash
+###        fi
+###    fi
+###    export ROS_WORKSPACE=$WS
+###else                            # no ROS workspace
+###    if [ -r /opt/ros/hydro/setup.bash ]
+###    then
+###        source /opt/ros/hydro/setup.bash
+###    else                        # ROS hydro not installed
+###        if [ -r /opt/ros/indigo/setup.bash ]
+###        then
+###            source /opt/ros/indigo/setup.bash
+###        fi
+###    fi
+###fi
